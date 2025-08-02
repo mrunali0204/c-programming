1f45d76ef8038d610ca2b210c90585c9f6cfd47a
mrunali0204 <mrunalijibhakate04@gmail.com>
2025-08-02 23:11:47 +0530

    Add Hello World program

---
+++ 00-helloworld/hello.s
@@ -0,0 +1,48 @@
+	.file	"hello.c"
+	.text
+	.globl	add
+	.def	add;	.scl	2;	.type	32;	.endef
+	.seh_proc	add
+add:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	.seh_endprologue
+	movl	%ecx, 16(%rbp)
+	movl	%edx, 24(%rbp)
+	nop
+	popq	%rbp
+	ret
+	.seh_endproc
+	.section .rdata,"dr"
+.LC0:
+	.ascii "%d\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	call	__main
+	movl	$2, %edx
+	movl	$1, %ecx
+	call	add
+	movl	%eax, %edx
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rcx
+	call	printf
+	movl	$0, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.ident	"GCC: (GNU) 15.1.0"
+	.def	printf;	.scl	2;	.type	32;	.endef
