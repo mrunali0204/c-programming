4b6cdcaf4962bc146e175840aa40cb44cea4aff8
Mrunali <mrunalijibhakate04@gmail.com>
2025-08-03 19:30:23 +0530

    added array to print marks of student

---
+++ 81-array1/ptr.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int marks[] = {27, 24, 34};
+    // or int marks[3] = {27, 24, 34}; 
+    printf("marks are: %d\n", marks[0]);
+    printf("marks are: %d\n", marks[1]);
+    printf("marks are: %d\n", marks[2]);
+}
+
+
+/* 
+w/o array
+int main(){
+    int marks1 =27;
+    int marks2 =24;
+    int marks3 =34;
+}
+
+*/
