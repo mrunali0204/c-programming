d4c79c52943d65d4c4a81c7636253f52e1a8d99b
Mrunali <mrunalijibhakate04@gmail.com>
2025-08-04 06:43:09 +0530

    added pointer arithmetic- case1- INT

--- 85-array5-PointerArithmetic-int/a.c
+++ 85-array5-PointerArithmetic-int/a.c
@@ -11,3 +11,16 @@ int main(){
 
     return 0;
 }
+
+/*
+output:
+
+ptr = 1163918919
+ptr = 1163918920
+ptr = 1163918919
+
+here int is 4 byte - so it is incremented by 4 byte for 1 datatype
+*/
+
+//ptr++
+//ptr = ptr + 1 -> 1 datatype increment, 4 bytes * 1 = 4 for int
