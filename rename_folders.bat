e65e456c08ebac1af89174745c64b917ac825d5d
Mrunali <mrunalijibhakate04@gmail.com>
2025-08-07 18:52:59 +0530

    Renamed folders correctly with leading zeros

--- rename_folders.bat
+++ rename_folders.bat
@@ -1,33 +1,18 @@
 @echo off
-setlocal enabledelayedexpansion
+SETLOCAL ENABLEDELAYEDEXPANSION
 
-REM Loop through all folders
-for /D %%f in (*) do (
-    set "name=%%f"
-
-    REM Extract the number prefix (before the first dash)
-    for /f "tokens=1* delims=-" %%a in ("%%f") do (
-        set "num=%%a"
-        set "rest=%%b"
-
-        REM Only process if num is a number
-        echo !num! | findstr /r "^[0-9][0-9]*$" >nul
-        if !errorlevel! == 0 (
-            if !num! LSS 10 (
-                set "newnum=00!num!"
-            ) else if !num! LSS 100 (
-                set "newnum=0!num!"
-            ) else (
-                set "newnum=!num!"
-            )
-            set "newname=!newnum!-!rest!"
-            if not "!newname!"=="%%f" (
-                echo Renaming %%f to !newname!
-                ren "%%f" "!newname!"
-            )
+:: Loop through all folders in current directory
+for /d %%f in (*) do (
+    set "folder=%%f"
+    set "prefix=!folder:~0,3!"
+    
+    REM Skip folders that already start with 3 digits (like 100-, 099-)
+    echo !prefix! | findstr "^[0-9][0-9][0-9]-" >nul
+    if errorlevel 1 (
+        for /f "tokens=1,* delims=-" %%a in ("%%f") do (
+            set "num=00%%a"
+            set "num=!num:~-3!"
+            ren "%%f" "!num!-%%b"
         )
     )
 )
-
-endlocal
-pause
