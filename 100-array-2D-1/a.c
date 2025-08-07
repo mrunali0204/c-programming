dbd464dc1dd690522104e23e0380b096e2747ea2
Mrunali <mrunalijibhakate04@gmail.com>
2025-08-07 07:21:16 +0530

    2d array

--- 100-array-2D-1/a.c
+++ 100-array-2D-1/a.c
@@ -1,18 +1,5 @@
-#include<stdio.h>
-
-void storeTable(int arr[][10], int n, int m, int number);
-
-int main(){
-    int tables[2][10];
-    storeTable(tables, 0, 10, 2);
-    storeTable(tables, 1,10, 3);
-    
-
+#include <stdio.h>
+int main() {
+    printf("Hello from 2D array file\n");
     return 0;
-}
-
-void storeTable(int arr[][10], int n, int m, int number){
-    for(int i =0; i<m; i++){
-        arr[n][i] = number*(i+1);
-    }
 }
