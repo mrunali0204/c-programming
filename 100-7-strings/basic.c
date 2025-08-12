b6e1aa809c239b9ed9a77bea0bc165adef330945
Mrunali <mrunalijibhakate04@gmail.com>
2025-08-12 07:45:39 +0530

    basic

---
+++ 100-7-strings/basic.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    char name[] = {'h', 'a', '\0'}; //this sees 0 and then it knows the array is ended
+    char name1[] = "har";
+    char class[] = "mrunali jibhakate";
+    printf( name);
+    printf("\n");
+    printf( name1);
+    printf("\n");
+    printf( class);
+}
+
