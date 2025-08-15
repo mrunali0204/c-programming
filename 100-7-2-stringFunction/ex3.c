3f5c4cb449e31fd374c2d931bc0637f644f86f96
Mrunali <mrunalijibhakate04@gmail.com>
2025-08-15 23:29:04 +0530

    struct

---
+++ 100-7-2-stringFunction/ex3.c
@@ -0,0 +1,9 @@
+struct Student {
+    char *name;
+};
+
+int main() {
+    struct Student s1;
+    s1.name = "mrunali"; // ✅ Just points to the literal (read-only memory)
+    printf("%s\n", s1.name);
+}
