ca8563c4af13e75d04c7c9b7b06d757abee2007a
Mrunali <mrunalijibhakate04@gmail.com>
2025-08-17 08:12:59 +0530

    passing structures to functions

---
+++ 100-7-5-structuresToFun/ex2.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+
+//void printInfo(struct student s1);  if this written here will give ERROR
+
+struct student{
+    int roll;
+    float cgpa;
+    char name[100];
+};
+
+void printInfo(struct student s1);
+
+int main(){
+    struct student s1 = {107, 9.5,"mrunali"};
+    printf("student roll = %d \n",s1.roll);
+    printInfo(s1);
+
+    //roll value changed here
+    s1.roll = 101; // output -> 101
+    printf("%d",s1.roll);
+    return 0;
+}
+
+void printInfo(struct student s1){
+    printf("student info : \n");
+    printf("student roll = %d \n",s1.roll);
+    printf("student cgpa = %f \n",s1.cgpa);
+    printf("student name = %s \n",s1.name);
+
+    //roll value changed here
+    s1.roll = 104; // output -> 101
+    printf("%d",s1.roll);
+}
