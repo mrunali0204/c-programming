ab38da0ba5191ac8ae7018e4c77a3a333a3911a6
Mrunali <mrunalijibhakate04@gmail.com>
2025-09-04 21:12:03 +0530

    .

--- 100-que/a5.c
+++ 100-que/a5.c
@@ -1,8 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <string.h>
 
-int main(){
+int main() {
     char str[100];
-    printf("enter the string: ");
-    scanf("%s", str);
+    int i, j, temp;
+
+    printf("Enter a string: ");
+    scanf("%s", str);   // Reads a single word (not spaces)
+
+    i = 0;
+    j = strlen(str) - 1;
+
+    while (i < j) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++;
+        j--;
+    }
+
+    printf("Reversed string: %s\n", str);
+
+    return 0;
 }
