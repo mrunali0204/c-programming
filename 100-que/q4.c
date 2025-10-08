81ce00cab7948b074c3f16dc7f38e7e55a5b9385
Mrunali <mrunalijibhakate04@gmail.com>
2025-10-08 22:56:16 +0530

    .

--- 100-que/q4.c
+++ 100-que/q4.c
@@ -7,25 +7,23 @@ bool isPalindrome(const char* s, int left, int right) {
     }
     return true;
 }
+
 char* longestPalindrome(char* s) {
     int len = strlen(s);
-    int start=0,size= 1;
+    int start = 0, size = 1;
 
-    for(int i = 1; i<len ; i++)
-    {
-        if (i - size >= 1 && isPalindrome(s, i - size - 1, i))
-        {
-            start = i-size-1;
+    for (int i = 1; i < len; i++) {
+        if (i - size >= 1 && isPalindrome(s, i - size - 1, i)) {
+            start = i - size - 1;
             size += 2;
-        }
-        
-        {
-            start = i-size;
+        } else if (isPalindrome(s, i - size, i)) {
+            start = i - size;
             size += 1;
         }
     }
-    char* result = (char*)malloc((size+1) * sizeof(char));
-    strncpy(result , s+start, size);
+
+    char* result = (char*)malloc((size + 1) * sizeof(char));
+    strncpy(result, s + start, size);
     result[size] = '\0';
     return result;
 }
