cc89386632afb85b257611cd2bafeec2cb15eda9
Mrunali <mrunalijibhakate04@gmail.com>
2025-10-10 21:55:26 +0530

    .

---
+++ 100-que/q6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+    printf("1%c\n", 65);
+    printf("2%s\n", 65);
+    printf("3%f\n", 65);
+    printf("4%d\n", 65);
+    
+    
+    // printf("5%c\n", A);
+    // printf("6%s\n", A);
+    // printf("7%f\n", A);
+    //printf("8%d\n", A);
+
+    return 0;
+
+}
