206f7d2556afd19ad39c564a9d4c09f704994669
Mrunali <mrunalijibhakate04@gmail.com>
2025-10-14 23:47:31 +0530

    .

--- 100-projects/project 1/a.c
+++ 100-projects/project 1/a.c
@@ -417,3 +417,45 @@ int main(void)
 *     *
 
 */
+
+#include<stdio.h>
+int main(void)
+{
+    for(int i = 0; i = 4; i++)
+    {
+        for(int j = 0; j = 4; j++)
+        {
+            if(i == 1 && j == 1 || i == 1 && j == 4)
+            {
+                printf("*");
+                printf("\n");
+            }
+            else if(i == 2 && j == 1 || i == 2 && j == 2 || i == 2 && j == 3 || i == 2 && j == 4)
+            {
+                printf("*");
+                printf("\n");
+            }
+            else if(i == 3 && j == 1 || i == 3 && j == 3 || i == 3 && j == 4)
+            {
+                printf("*");
+                printf("\n");
+            }
+            else if(i == 4 && j == 1 || i == 4 && j == 4)
+            {
+                printf("*");
+                printf("\n");
+            }
+        }
+    }
+
+    return 0;
+}
+
+/*
+
+*     *
+* * * *
+*  *  *
+*     *
+
+*/
