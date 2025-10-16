62699e64b85a07dfe913a661f37b067ca1c6aa24
Mrunali <mrunalijibhakate04@gmail.com>
2025-10-16 20:18:17 +0530

    .

--- 100-projects/project 1 - simple calculator/calculator.c
+++ 100-projects/project 1 - simple calculator/calculator.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
 int main(void)
-{
+{   
+
+    int choose;
+    printf("choose one from the following : \n");
+    printf("1. Addition\n 2. Substraction\n 3. Multiplication\n 4. Division\n 5. Modulus\n 6.  : \n");
+    scanf("%d", &choose);
+
+    int iNo1, iNo2;
+    printf("enter the two values : \n");
+    scanf("%d%d", &iNo1, &iNo2);
+
 
     return 0;
 }
