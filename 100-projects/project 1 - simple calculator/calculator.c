ea12cf7b13bc58a90af343660c1e45cf88f87ae1
Mrunali <mrunalijibhakate04@gmail.com>
2025-10-18 23:04:04 +0530

    .

--- 100-projects/project 1 - simple calculator/calculator.c
+++ 100-projects/project 1 - simple calculator/calculator.c
@@ -2,6 +2,7 @@
 #include<math.h>
 
 int simple_calculator(int iChoose, int iNo1, int iNo2);
+int scientific_calculator(int iChoose, int iNo1, int iNo2);
 
 int main(void)
 {
@@ -9,10 +10,6 @@ int main(void)
     int iChoose;
     int iNo1;            //declare here bcoz old compiler gives error
     int iNo2;
-
-    printf("choose one from the following : \n");
-    printf(" 1. Addition\n 2. Substraction\n 3. Multiplication\n 4. Division\n 5. Modulus\n 6. Square\n 7. SquareRoot \n");
-    scanf("%d", &iChoose);
 
     //int iNo1;           //calculator.c(15) : error C2143: syntax error : missing ';' before 'type'
     //int iNo2;          //calculator.c(16) : error C2143: syntax error : missing ';' before 'type'
@@ -22,12 +19,18 @@ int main(void)
 
     simple_calculator(iChoose, iNo1, iNo2);
 
+    //scientific_calculator(iChoose, iNo1, iNo2);
+
     return 0;
 }
 
 
 int simple_calculator(int iChoose, int iNo1, int iNo2)
 {
+    printf("choose one from the following : \n");
+    printf(" 1. Addition\n 2. Substraction\n 3. Multiplication\n 4. Division\n 5. Modulus\n 6. Square\n 7. SquareRoot \n");
+    scanf("%d", &iChoose);
+
     switch(iChoose){
         case 1:
         printf("1. Addition of two values : %d\n", iNo1 + iNo2);
@@ -46,22 +49,93 @@ int simple_calculator(int iChoose, int iNo1, int iNo2)
         break;
 
         case 5:
-        printf("5. Modulus of two values : %d\n", iNo1 % iNo2);
+        printf("5. Modulus of two values : %d, %d\n", iNo1 % iNo2);
         break;
 
         case 6:
-        printf("6. Square of two values : %d\n", iNo1 * iNo1, iNo2 * iNo2);
+        printf("6. Square of two values : %d, %d\n", iNo1 * iNo1, iNo2 * iNo2);
         break;
 
         case 7:
-        printf("7. SquareRoot of two values : %d\n", sqrt(iNo1), sqrt(iNo2));   //sqrt
+        printf("7. SquareRoot of two values : %d, %d\n", sqrt(iNo1), sqrt(iNo2));   //sqrt
         break;
 
         default:
         printf("Invalid");
         break;
         
+        return iChoose;
     }
 }
 
+// int scientific_calculator(int iChoose, int iNo1, int iNo2)
+// {
+//     int iSelect;
+
+//     switch(iChoose){
+//         case 1:
+//             switch(iSelect){
+//                 printf("1. Trigonometric of values : %d\n", iNo1 , iNo2);
+
+//                 case 1:
+//                 printf("1. Trigonometric 2^nd of two values : %d\n", pow(2, iNo1), pow(2, iNo2));
+//                 break;
+
+//                 case 2:
+//                 printf("2. Trigonometric sin of two values : %d\n", sin(iNo1), sin(iNo2));
+//                 break;
+
+//                 case 3:
+//                 printf("3. Trigonometric cos of two values : %d\n", cos(iNo1), cos(iNo2));
+//                 break;
+//             }
+
+//         case 2:
+//         switch(iSelect){
+//                 printf("1. Trigonometric of values : %d\n", iNo1 , iNo2);
+
+//                 case 1:
+//                 printf("1. Trigonometric 2^nd of two values : %d\n", pow(2, iNo1), pow(2, iNo2));
+//                 break;
+
+//                 case 2:
+//                 printf("2. Trigonometric sin of two values : %d\n", sin(iNo1), sin(iNo2));
+//                 break;
+
+//                 case 3:
+//                 printf("3. Trigonometric cos of two values : %d\n", cos(iNo1), cos(iNo2));
+//                 break;
+
+//                 default:
+//                 printf("Invalid");
+//                 break;
+//             }
+        
+//         case 3:
+//         switch(iSelect){
+//                 printf("1. Trigonometric of values : %d\n", iNo1 , iNo2);
+
+//                 case 1:
+//                 printf("1. Trigonometric 2^nd of two values : %d\n", pow(2, iNo1), pow(2, iNo2));
+//                 break;
+
+//                 case 2:
+//                 printf("2. Trigonometric sin of two values : %d\n", sin(iNo1), sin(iNo2));
+//                 break;
+
+//                 case 3:
+//                 printf("3. Trigonometric cos of two values : %d\n", cos(iNo1), cos(iNo2));
+//                 break;
+
+//                 default:
+//                 printf("Invalid");
+//                 break;
+//             }
+        
+//         default:
+//         printf("Invalid");
+//         break;
+        
+//     }
+// }
 
