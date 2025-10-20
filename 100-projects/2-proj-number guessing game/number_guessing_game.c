ac37c275861927976dd4ccddf32da63ae4460722
Mrunali <mrunalijibhakate04@gmail.com>
2025-10-20 19:22:31 +0530

    .

---
+++ 100-projects/2-proj-number guessing game/number_guessing_game.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int iNo;
+    printf("Enter any number: \n");
+    scanf("%d", &iNo);
+
+    if(iNo > 100)
+    {
+        printf("Number is too High\n");
+    }
+    else if(iNo < 100)
+    {
+        printf("Number is too Low\n");
+    }
+    else{
+        printf("Invalid");
+    }
+}
